3a7fc75d16809ff3107e003f358856d2c9ac239b
lbryyds-del <l826604001@gmail.com>
2026-08-28 17:07:17 +0800

    Register Query-class matchability extension

--- trokens/models/__init__.py
+++ trokens/models/__init__.py
@@ -3,3 +3,8 @@
 
 from .build import MODEL_REGISTRY, build_model  # noqa
 from .pointformer import *  # noqa
+from .query_class_matchability import install_query_class_matchability
+
+# Install the optional 8.64 extension after Pointformer is registered. The
+# original builder remains active whenever QUERY_CLASS_MATCHABILITY is disabled.
+install_query_class_matchability(Pointformer)
